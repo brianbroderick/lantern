03762815157cc62f39793fc7c7e08582c43d8def
Brian Broderick <brianbroderick@users.noreply.github.com>
2023-08-15 14:46:21 -0600

    add an alias to the test

--- internal/sql/ast/ast_test.go
+++ internal/sql/ast/ast_test.go
@@ -28,3 +28,31 @@ func TestString(t *testing.T) {
 		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
+
+// This tests that the ColumnExpression is working with an Identifier value and an alias.
+func TestColumn(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&SelectStatement{
+				Token: token.Token{Type: token.SELECT, Lit: "select"},
+				Columns: &ColumnExpression{
+					Token: token.Token{Type: token.IDENT, Lit: "id"},
+					Value: &Identifier{
+						Token: token.Token{Type: token.IDENT, Lit: "id"},
+						Value: "id",
+					},
+					Alias: "user_id",
+				},
+
+				From: &Identifier{
+					Token: token.Token{Type: token.IDENT, Lit: "users"},
+					Value: "users",
+				},
+			},
+		},
+	}
+
+	if program.String() != "select id as user_id from users;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
