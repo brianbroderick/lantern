692f560f4c6fa251f634d968f8654711988125f8
Brian Broderick <brianbroderick@users.noreply.github.com>
2023-12-04 13:27:54 -0700

    start with update statements, rename some files to have the word parser in them

---
+++ pkg/sql/ast/update_ast.go
@@ -0,0 +1,100 @@
+package ast
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/brianbroderick/lantern/pkg/sql/token"
+)
+
+type UpdateStatement struct {
+	Token      token.Token `json:"token,omitempty"` // the token.UPDATE token
+	Expression Expression  `json:"expression,omitempty"`
+}
+
+func (s *UpdateStatement) statementNode()       {}
+func (s *UpdateStatement) TokenLiteral() string { return s.Token.Upper }
+func (s *UpdateStatement) String(maskParams bool) string {
+	var out bytes.Buffer
+	out.WriteString(s.Expression.String(maskParams))
+	out.WriteString(";")
+	return out.String()
+}
+func (s *UpdateStatement) Inspect(maskParams bool) string {
+	j, err := json.MarshalIndent(s, "", "  ")
+	if err != nil {
+		fmt.Printf("Error loading data: %#v\n\n", err)
+	}
+	return string(j)
+}
+
+type UpdateExpression struct {
+	Token     token.Token  `json:"token,omitempty"` // the token.UPDATE token
+	Only      bool         `json:"only,omitempty"`
+	Table     Expression   `json:"table,omitempty"`
+	Asterisk  bool         `json:"asterisk,omitempty"`
+	Alias     Expression   `json:"alias,omitempty"`
+	Set       []Expression `json:"set,omitempty"`
+	From      []Expression `json:"from,omitempty"`
+	Where     Expression   `json:"where,omitempty"`
+	Returning []Expression `json:"returning,omitempty"`
+	Cast      Expression   `json:"cast,omitempty"`
+}
+
+func (x *UpdateExpression) expressionNode()      {}
+func (x *UpdateExpression) TokenLiteral() string { return x.Token.Upper }
+func (x *UpdateExpression) SetCast(cast Expression) {
+	x.Cast = cast
+}
+
+func (x *UpdateExpression) String(maskParams bool) string {
+	var out bytes.Buffer
+	out.WriteString("(UPDATE ")
+
+	if x.Only {
+		out.WriteString("ONLY ")
+	}
+	if x.Table != nil {
+		out.WriteString(x.Table.String(maskParams))
+	}
+	if x.Asterisk {
+		out.WriteString(" *")
+	}
+	if x.Alias != nil {
+		out.WriteString(" AS ")
+		out.WriteString(x.Alias.String(maskParams))
+	}
+	if len(x.Set) > 0 {
+		out.WriteString(" SET ")
+		for i, s := range x.Set {
+			if i > 0 {
+				out.WriteString(", ")
+			}
+			out.WriteString(s.String(maskParams))
+		}
+	}
+	if len(x.From) > 0 {
+		out.WriteString(" FROM ")
+		for i, f := range x.From {
+			if i > 0 {
+				out.WriteString(", ")
+			}
+			out.WriteString(f.String(maskParams))
+		}
+	}
+	if x.Where != nil {
+		out.WriteString(" WHERE ")
+		out.WriteString(x.Where.String(maskParams))
+	}
+	if len(x.Returning) > 0 {
+		out.WriteString(" RETURNING ")
+		for i, r := range x.Returning {
+			if i > 0 {
+				out.WriteString(", ")
+			}
+			out.WriteString(r.String(maskParams))
+		}
+	}
+	return out.String()
+}
