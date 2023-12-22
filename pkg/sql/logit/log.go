0fbe709fd3db09363cf7001f94ba36c700f9e635
Brian Broderick <brianbroderick@users.noreply.github.com>
2023-12-22 11:16:22 -0700

    refining logit

--- pkg/sql/logit/log.go
+++ pkg/sql/logit/log.go
@@ -10,11 +10,11 @@ import (
 	"github.com/brianbroderick/lantern/pkg/sql/projectpath"
 )
 
-func Append(str string) {
+func Append(filePrefix, str string) {
 	now := time.Now()
 	date := now.Format("2006-01-02")
 
-	f, err := os.OpenFile(filepath.Join(projectpath.Root, "logs", fmt.Sprintf("test-%s.log", date)),
+	f, err := os.OpenFile(filepath.Join(projectpath.Root, "logs", fmt.Sprintf("%s-%s.log", filePrefix, date)),
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println(err)
--- pkg/sql/logit/log_test.go
+++ pkg/sql/logit/log_test.go
@@ -1,20 +1,18 @@
 package logit
 
-import (
-	"fmt"
-	"testing"
-	"time"
-)
+// We don't need to run this all the time, but leaving it commented
+// in the file in case we need to test it again.
 
-func TestLog(t *testing.T) {
-	t1 := time.Now()
+// func TestLog(t *testing.T) {
+// 	batch := 1000
+// 	t1 := time.Now()
 
-	for i := 0; i < 100000; i++ {
-		Append(fmt.Sprintf("%d: Hello World", i))
-	}
+// 	for i := 0; i < batch; i++ {
+// 		Append("test", fmt.Sprintf("%d: Hello World", i))
+// 	}
 
-	t2 := time.Now()
-	timeDiff := t2.Sub(t1)
-	avg := timeDiff / time.Duration(100000)
-	fmt.Printf("TestLog, Elapsed Time: %s, Avg per line: %s\n", timeDiff, avg)
-}
+// 	t2 := time.Now()
+// 	timeDiff := t2.Sub(t1)
+// 	avg := timeDiff / time.Duration(batch)
+// 	fmt.Printf("TestLog, Elapsed Time: %s, Avg per line: %s\n", timeDiff, avg)
+// }
