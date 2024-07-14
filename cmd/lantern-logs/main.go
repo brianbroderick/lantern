720be03b68eee2f7b8c58a17ee6ceacb2df13d11
Brian Broderick <brianbroderick@users.noreply.github.com>
2024-07-14 17:13:39 -0600

    process logs from cli

---
+++ cmd/lantern-logs/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/brianbroderick/lantern/internal/postgresql/logs"
+
+func main() {
+	// logs.AggregateLogs()
+	logs.UpsertQueries()
+	logs.UpsertDatabases()
+	logs.ExtractAndUpsertQueryMetadata()
+}
